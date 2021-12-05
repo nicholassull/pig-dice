0d2b7319777b81d686417ab607463dfcee6ddb52
Nick Sullivan <nicholassull@gmail.com>
2021-12-05 10:36:45 -0800

    add initial HTML play areas and fullGame()
    co-authored-by: Elliot McGonigal <elliot@elliotmcgonigal.com>
    co-authored-by: Phil Curran <pecurran@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+//Utility functions
 
 function dieRoll() {
-  return (getRandomInt(6) + 1);
+  return Math.floor(Math.random() * 6) + 1;
 }
 
 function Player() {
-  this.score = 0;
+  this.totalScore = 0;
+  this.roundScore = 0;
   this.turn = false;
+}
+
+//Business Logic
+function fullGame() {
+  let playerOne = new Player();
+  let playerTwo = new Player();
+  if (dieRoll() % 2 === 0) {
+    playerOne.turn = true;
+  } else {
+    playerTwo.turn = true;
+  }
+  console.log(playerTwo.turn);
+  console.log(playerOne.turn);
 }
 
 
@@ -16,3 +28,10 @@ function Player() {
 //Test to see if the turn is over
 //Test to see if game is over
 //Add dice to score only if you ended your turn
+
+//User Interface Logic
+$(document).ready(function() {
+  $("#play-button").click(function() {
+    
+  });
+});
