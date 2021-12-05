569d82425aa036540b6fc4db7e0403f723357836
Nick Sullivan <nicholassull@gmail.com>
2021-12-05 15:22:30 -0800

    create roll function which toggles display of buttons on a 1 roll
    co-authored-by: Elliot McGonigal <elliot@elliotmcgonigal.com>
    co-authored-by: Phil Curran <pecurran@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,72 +13,29 @@ function gameTurn() {
   let roundScore = 0;
 }
 
-function fullGame() {
-  let playerOne = new Player();
-  let playerTwo = new Player();
-    
+let playerOne = new Player();
+let playerTwo = new Player();
+let roundScore = 0;
+
+function newGame() {
+  playerOne = new Player();
+  playerTwo = new Player(); 
+  roundScore = 0; 
 }
 
-
-//Take a turn
-//Test to see if the turn is over
-//Test to see if game is over
-//Add dice to score only if you ended your turn
 
 //User Interface Logic
 $(document).ready(function() {
-
-  $("#play-button").click(function() {
-    fullGame();
-    // if (gameResult === 1) {
-      
-    // }
-    // else if (gameResult === 2) {
-
-    // }
-    let roll = 0;
-    let turnScore = 0;
-    while (roll !== 1) {
-      roll = dieRoll();
-      if (roll === 1) {
-        turnScore = 0;
-      }
-      else if (roll !== 1) {
-        turnScore += roll;
-      }
+  $("#playerOneRollDice").click(function() {
+    let roll = dieRoll();
+    if (roll !== 1) {
+      roundScore += roll;
+      $("#playerOneRollHistory").append("<li>" + roll + "</li>");
+    } else {
+      roundScore = 0
+      $("#playerOneRollHistory").append("<li>" + roll + "</li>");
+      $(".playerOneZone").toggle();
+      $(".playerTwoZone").toggle();
     }
   });
 });
-
-// start document.ready with player instantiation
-// currently, cannot access any player data with playerOne.score or fullGame.playerOne.xxx
-// but if I do let phil = new Player(); I can access phil.score, etc.
-// I think we need a gameTurn function that takes the player as an arg
-// then sets vars as currentScore = 0; totalScore = imported as arg.totalScore
-
-//Business Logic
-function gameTurn(player) {
-  let roundScore = 0;
-  let totalScore = player.score;
-  player.turn = true;
-  while (player.turn = true) {
-
-  }
-  $('.gamePlay').click(function(event) {
-    let temp = dieRoll();
-    if (temp === 1) {
-      roundscore = 0;
-      player.turn = false;
-    } else {
-      roundScore += temp;
-      $('.buttonReveal').toggle();
-    }
-    $('#buttonContinue').click(function(event) {
-    //continue loop
-    });
-    $('#buttonStop').click(function(event) {
-    //break loop
-    });
-  });
-
-}
